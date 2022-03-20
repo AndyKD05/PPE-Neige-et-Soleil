1968c24f91580fd25635357a3d642cb86b3f55db
AndyKD05 <39829615+AndyKD05@users.noreply.github.com>
2022-03-20 15:03:39 +0100

    Update_connexion_bdd_ionos

--- controleur/config_bdd.php
+++ controleur/config_bdd.php
@@ -1,7 +1,21 @@
-<?php
-	$serveur ="localhost";
-	$bdd = "neigesoleil";
-	$user="root";
-	$mdp="";
+<?php 
+//error_reporting(E_ALL);
+//ini_set('display_errors','On');
 
+$serveur = $_SERVER['SERVER_NAME']; 
+if ($_SERVER['SERVER_NAME']=="127.0.0.1" || $_SERVER['SERVER_NAME']=="localhost")
+   { 
+   //$serveur ="localhost:3306"; 
+   $bdd = "neigesoleil";
+   $user="root";
+   $mdp=""; 
+   } 
+  else // 1and1
+   { 
+   $serveur ="db5006845924.hosting-data.io";
+   $bdd = "dbs5651848";
+   $user="dbu2909969";
+   $mdp="Wanuke70"; 
+  }   
+  echo "<br/><h2>-------------serveur=".$serveur.", bdd=".$bdd.", user=".$user."</h2>";
 ?>
