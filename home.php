490d929b5c8671ffcb45035067e90ee978c0d787
AndyKD05 <39829615+AndyKD05@users.noreply.github.com>
2022-05-31 01:14:46 +0200

    update

--- home.php
+++ home.php
@@ -1,2 +1,107 @@
+ 
+<head>    
+  <style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        user-select: none;
+      }
 
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+    </style>
+  </head>
+
+
+
+
+
+
+
+  <div id="myCarousel" class="carousel slide" data-bs-ride="carousel">
+    <div class="carousel-inner">
+
+      <div class="carousel-item active">
+        <img class="img-fluid" src="images/paysage1.png">
+        <div class="container">
+          <div class="carousel-caption text-start">
+            <h1>Vallée du Queyras</h1>
+            <p>Randonnée dans la vallée du Queyras. Situé dans le nord-est du département des Hautes-Alpes, le Parc naturel régional du Queyras est le paradis des randonneurs.</p>
+          </div>
+        </div>
+      </div>
+
+      <div class="carousel-item">
+        <img class="img-fluid" src="images/maison3.png">
+
+        <div class="container">
+          <div class="carousel-caption">
+            <h1>Maison du Lac</h1>
+            <p>Rien de plus authentique que les chalets au cœur de la montagne pour profiter au maximum. Vous cherchez un Chalet à louer ?</p>
+          </div>
+        </div>
+      </div>
+
+      <div class="carousel-item">
+        <img class="img-fluid" src="images/paysage2.png">
+
+        <div class="container">
+          <div class="carousel-caption text-end">
+            <h1>Chalet Montagne du Queyras</h1>
+            <p>Chalet dont le bois est le matériau de construction essentiel.</p>
+          </div>
+        </div>
+      </div>
+
+    </div>
+
+    <button class="carousel-control-prev" type="button" data-bs-target="#myCarousel" data-bs-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#myCarousel" data-bs-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    </button>
+  </div>
+
+</br>
+
+  <div class="container">
+    <div class="row mb-2">
+      <div class="col-md-6">
+        <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+          <div class="col p-4 d-flex flex-column position-static">
+            <strong class="d-inline-block mb-2 text-primary">Chalet</strong>
+            <h3 class="mb-0">Bord de Lac</h3>
+            <div class="mb-1 text-muted">Nov 12</div>
+              <p class="card-text mb-auto" align="justify">Maison de campagne au bord de l'eau. Location Chalet Queyras, pour 6 personnes.</p>
+              <a href="#" class="stretched-link">Continue reading</a>
+            </div>
+          <div class="col-auto d-none d-lg-block">
+         
+           <img class="bd-placeholder-img" width="200" height="200" role="img"  src="images/maison4.png">
+
+          </div>
+        </div>
+      </div>
+      <div class="col-md-6">
+        <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+          <div class="col p-4 d-flex flex-column position-static">
+            <strong class="d-inline-block mb-2 text-success">Queyras</strong>
+            <h3 class="mb-0">Maison de Montagne</h3>
+            <div class="mb-1 text-muted">Nov 11</div>
+            <p class="mb-auto" align="justify">La location de vacances de vos rêves, avec tout le nécessaire pour un séjour sans stress.</p>
+            <a href="#" class="stretched-link">Continue reading</a>
+          </div>
+          <div class="col-auto d-none d-lg-block">
+            <img class="bd-placeholder-img" width="200" height="200" role="img"  src="images/maison4.png">
+            </div>
+          </div>
+        </div>
+      </div>
+  </div>
 
