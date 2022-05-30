a59d8e16d44376918dd082f664c3bd92c89ab369
AndyKD05 <39829615+AndyKD05@users.noreply.github.com>
2022-05-31 01:30:50 +0200

    Update gestion_inscription_proprietaire.php

--- gestion_inscription_proprietaire.php
+++ gestion_inscription_proprietaire.php
@@ -1,10 +1,10 @@
-<h2>Gestion des proprietaires</h2>
+
 <?php
 $pass = '/(?=\S{8,})(?=\S*[A-Z])(?=\S*[\d])(?=\S*[\W])/';
 $nom = '/^[^@&"()!_$*€£`+=\/;?#]+$/';
 
 	$unControleur->setTable("proprietaire");
-	require_once("vue/vue_inscription_Proprietaire.php");
+	require_once("vue/vue_inscription_proprietaire.php");
 	if(isset($_POST['Inscrire']))
 	{
 		if(preg_match($pass, $_POST['mdp_p']) and preg_match($nom, $_POST['nom_p']) and preg_match($nom, $_POST['prenom_p'])){
