ea7e6c388e21b2f3d2d22feeb13a99866c21ccf0
AndyKD05 <39829615+AndyKD05@users.noreply.github.com>
2022-05-31 01:35:37 +0200

    Update config_bdd.php

--- controleur/config_bdd.php
+++ controleur/config_bdd.php
@@ -1,6 +1,7 @@
 <?php 
-//error_reporting(E_ALL);
-//ini_set('display_errors','On');
+
+error_reporting(E_ALL);
+ini_set('display_errors','On');
 
 $serveur = $_SERVER['SERVER_NAME']; 
 if ($_SERVER['SERVER_NAME']=="127.0.0.1" || $_SERVER['SERVER_NAME']=="localhost")
@@ -19,3 +20,42 @@ if ($_SERVER['SERVER_NAME']=="127.0.0.1" || $_SERVER['SERVER_NAME']=="localhost"
   }   
   //echo "<br/><h2>-------------serveur=".$serveur.", bdd=".$bdd.", user=".$user."</h2>";
 ?>
+<!-- JavaScript code -->
+<script>
+  
+  /* Code for changing active 
+  link on clicking */
+  var btns = 
+      $("#navigation .navbar-nav .nav-link");
+
+  for (var i = 0; i < btns.length; i++) {
+      btns[i].addEventListener("click",
+                            function () {
+          var current = document
+              .getElementsByClassName("active");
+
+          current[0].className = current[0]
+              .className.replace(" active", "");
+
+          this.className += " active";
+      });
+  }
+
+  /* Code for changing active 
+  link on Scrolling */
+  $(window).scroll(function () {
+      var distance = $(window).scrollTop();
+      $('.page-section').each(function (i) {
+
+          if ($(this).position().top 
+              <= distance + 250) {
+                
+                  $('.navbar-nav a.active')
+                      .removeClass('active');
+
+                  $('.navbar-nav a').eq(i)
+                      .addClass('active');
+          }
+      });
+  }).scroll();
+</script>
